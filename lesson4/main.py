bef8e292d9375e04176967d065bba9ec431ab0b7
Artalex <artalexit@gmail.com>
2024-02-11 10:25:25 +0500

    refactoring exercise_one

--- lesson4/main.py
+++ lesson4/main.py
@@ -15,24 +15,18 @@
 # Функция возвращает тип данных str  1234567 -> 1 234 567, 267 -> 267, 34976 -> 34 976`
 def exercise_one() -> str:
     num_str = str(input("Введите целое число: "))
-    length = len(str(num_str))
-    result = ''
-    ind = 0
-    while length % 3 != 0:
-        ind += 1
-        length -= 1
-    else:
-        if ind != 0:
-            result = result + num_str[:ind] + ' '
-        for i in range(ind, length, 3):
-            result = result + num_str[i:i + 3] + ' '
-    return result
+    length_num_str = len(num_str)
+    first_rank_num_str = ''.join(
+        [num_str[:i] for i in range(0, length_num_str - 1, 1) if (length_num_str - i) % 3 == 0 and i <= 2])
+    length_first_rank_num_str = len(first_rank_num_str)
+    other_rank_num_str = ' '.join([num_str[i:i + 3] for i in range(length_first_rank_num_str, length_num_str - 1, 1) if
+                                   (length_num_str - i) % 3 == 0])
+    return first_rank_num_str + ' ' + other_rank_num_str
 
 
 # Задание 2
 # Написать функцию, которая заменяет принимает строку текста и изменяет снейк_кейс на КамелКейс и наоборот
 # my_first_func -> MyFirstFunc, AnotherFunction -> nother_function
-
 def exercise_two() -> str:
     return ''
 
@@ -40,7 +34,6 @@ def exercise_two() -> str:
 # Задание 3
 # Написать функцию, которая принимает на вход квадратное уравнение (одной строкой) и возвращает его корни,
 # либо сообщает, что их нет "x**2 - 11*x + 28 = 0" -> x_1 = 4, x_2 = 7
-
 def exercise_three():
     pass
 
@@ -56,6 +49,6 @@ def exercise_four():
 
 if __name__ == '__main__':
     print(exercise_one())
-    print(exercise_two())
-    print(exercise_three())
-    print(exercise_four())
+print(exercise_two())
+print(exercise_three())
+print(exercise_four())
