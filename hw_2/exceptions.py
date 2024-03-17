911da3856603d748edecd59bc735e8ca6ebb8db0
artalex <itartalex@gmail.com>
2024-03-17 22:31:35 +0500

    added classes Vehicle, Engine, Car, Plane

--- hw_2/exceptions.py
+++ hw_2/exceptions.py
@@ -4,3 +4,25 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+    def __init__(self, msg, *args):
+        super().__init__(args)
+        self.msg = msg
+
+    def __str__(self):
+        return f'{self.msg}'
+
+
+class NotEnoughFuel(Exception):
+    def __init__(self, msg, *args):
+        super().__init__(args)
+        self.msg = msg
+
+    def __str__(self):
+        return f'{self.msg}'
+
+
+class CargoOverload(Exception):
+    pass
